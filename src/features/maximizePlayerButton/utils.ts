YouTube-Enhancer/extension

refactor(maximize player): WIP re-write

src/features/maximizePlayerButton/utils.ts
@@ -1,9 +1,8 @@
-import type { YouTubePlayerDiv } from "@/src/types";
-
 import eventManager from "@/src/utils/EventManager";
 import { createSVGElement } from "@/src/utils/utilities";
-let wasInTheatreMode = false;
-let setToTheatreMode = false;
+
+import { getFeatureMenuItem } from "../featureMenu/utils";
+
 export function makeMaximizeSVG(): SVGElement {
 	const maximizeSVG = createSVGElement(
 		"svg",
@@ -44,106 +43,74 @@ export function updateProgressBarPositions() {
 	const duration = parseInt(seekBar?.ariaValueMax ?? "0") ?? 0;
 	const seekBarWidth = seekBar?.clientWidth ?? 0;
 
-	const scrubberPosition = parseFloat(Math.min((elapsedTime / duration) * seekBarWidth, seekBarWidth).toFixed(3));
-	// TODO: get this working when video is playing
+	const scrubberPosition = Math.ceil(parseFloat(Math.min((elapsedTime / duration) * seekBarWidth, seekBarWidth).toFixed(1)));
 	scrubber.style.transform = `translateX(${scrubberPosition}px)`;
-	// TODO: get all progress bar lists updated when video is playing
 	hoverProgress.style.left = `${scrubberPosition}px`;
 }
-export function setupVideoPlayerTimeUpdate() {
-	const videoElement = document.querySelector<HTMLVideoElement>("video.video-stream.html5-main-video");
-	if (!videoElement) return;
-	const videoPlayerTimeUpdateListener = () => {
-		updateProgressBarPositions();
-	};
-	eventManager.addEventListener(videoElement, "timeupdate", videoPlayerTimeUpdateListener, "maximizePlayerButton");
-}
-export function maximizePlayer() {
-	// Get the video element
-	const videoElement = document.querySelector<HTMLVideoElement>("video.video-stream.html5-main-video");
-	// If video element is not available, return
-	if (!videoElement) return;
-	const videoContainer = document.querySelector<YouTubePlayerDiv>("#movie_player");
-	if (!videoContainer) return;
-	const controlsElement = document.querySelector<HTMLDivElement>("div.ytp-chrome-bottom");
-	if (!controlsElement) return;
-	const sizeElement = document.querySelector("button.ytp-button.ytp-size-button");
-	if (!sizeElement) return;
-	const featureMenu = document.querySelector<HTMLDivElement>("#yte-feature-menu");
-	if (!featureMenu) return;
-	const {
-		childNodes: [svgElement]
-	} = sizeElement;
-	if (!svgElement || !(svgElement instanceof SVGElement)) return;
-	const theaterModeVariables = {
-		ariaLabel: "Theater mode keyboard shortcut t",
-		dataTitleNoTooltip: "Theater mode",
-		pathD: "m 28,11 0,14 -20,0 0,-14 z m -18,2 16,0 0,10 -16,0 0,-10 z",
-		title: "Theater mode (t)"
-	};
-	const defaultModeVariables = {
-		ariaLabel: "Default view keyboard shortcut t",
-		dataTitleNoTooltip: "Default view",
-		pathD: "m 26,13 0,10 -16,0 0,-10 z m -14,2 12,0 0,6 -12,0 0,-6 z",
-		title: "Default view (t)"
-	};
-	const {
-		childNodes: [, svgPath]
-	} = svgElement;
-	if (!svgPath || !(svgPath instanceof SVGPathElement)) return;
-	if (svgPath.getAttribute("d") === theaterModeVariables.pathD) wasInTheatreMode = true;
-	else wasInTheatreMode = false;
-	if (wasInTheatreMode) setToTheatreMode = false;
-	else setToTheatreMode = true;
-	// TODO: finish this code to make the maximize player button work properly. (implement ytp-scrubber-container adjustment) if all else fails revert to using the theatre mode button to get the tooltips in the correct place
-	console.log(`setToTheatreMode && wasInTheatreMode: ${setToTheatreMode && wasInTheatreMode}`);
-	console.log(`setToTheatreMode && !wasInTheatreMode: ${setToTheatreMode && !wasInTheatreMode}`);
-	console.log(`!setToTheatreMode && wasInTheatreMode: ${!setToTheatreMode && wasInTheatreMode}`);
-	console.log(`!setToTheatreMode && !wasInTheatreMode: ${!setToTheatreMode && !wasInTheatreMode}`);
-	if (
-		videoContainer.classList.contains("maximized_video_container") &&
-		videoElement.classList.contains("maximized_video") &&
-		controlsElement.classList.contains("maximized_controls")
-	) {
-		// sizeElement.click();
-		// if (setToTheatreMode && wasInTheatreMode) {
-		// 	const { dataTitleNoTooltip, title, ariaLabel, pathD } = defaultModeVariables;
-		// 	svgPath.setAttributeNS(null, "d", pathD);
-		// 	sizeElement.setAttributeNS(null, "aria-label", ariaLabel);
-		// 	sizeElement.dataset.titleNoTooltip = dataTitleNoTooltip;
-		// 	sizeElement.title = title;
-		// } else if (setToTheatreMode && !wasInTheatreMode) {
-		// 	const { dataTitleNoTooltip, title, ariaLabel, pathD } = theaterModeVariables;
-		// 	svgPath.setAttributeNS(null, "d", pathD);
-		// 	sizeElement.setAttributeNS(null, "aria-label", ariaLabel);
-		// 	sizeElement.dataset.titleNoTooltip = dataTitleNoTooltip;
-		// 	sizeElement.title = title;
-		// }
-		document.body.style.overflow = "";
-		videoElement.classList.remove("maximized_video");
-		videoContainer.classList.remove("maximized_video_container");
-		controlsElement.classList.remove("maximized_controls");
-		featureMenu.style.bottom = "";
-	} else {
-		// sizeElement.click();
 
-		// if (!setToTheatreMode && wasInTheatreMode) {
-		// 	const { dataTitleNoTooltip, title, ariaLabel, pathD } = defaultModeVariables;
-		// 	svgPath.setAttributeNS(null, "d", pathD);
-		// 	sizeElement.setAttributeNS(null, "aria-label", ariaLabel);
-		// 	sizeElement.dataset.titleNoTooltip = dataTitleNoTooltip;
-		// 	sizeElement.title = title;
-		// } else if (setToTheatreMode && !wasInTheatreMode) {
-		// 	const { dataTitleNoTooltip, title, ariaLabel, pathD } = theaterModeVariables;
-		// 	svgPath.setAttributeNS(null, "d", pathD);
-		// 	sizeElement.setAttributeNS(null, "aria-label", ariaLabel);
-		// 	sizeElement.dataset.titleNoTooltip = dataTitleNoTooltip;
-		// 	sizeElement.title = title;
-		// }
-		document.body.style.overflow = "hidden";
-		videoElement.classList.add("maximized_video");
-		videoContainer.classList.add("maximized_video_container");
-		controlsElement.classList.add("maximized_controls");
-		featureMenu.style.bottom = "-14px";
-	}
+type Action = "add" | "remove";
+type ElementClassPair = { className: string; selector: string };
+function modifyElementClassList(action: Action, elementPair: ElementClassPair) {
+	const { className, selector } = elementPair;
+	const element = document.querySelector<HTMLElement>(selector);
+	if (!element) return;
+	element.classList[action](className);
+}
+function modifyElementsClassList(action: Action, elements: ElementClassPair[]) {
+	elements.forEach((element) => modifyElementClassList(action, element));
+}
+function adjustPlayer(action: Action) {
+	const elements: ElementClassPair[] = [
+		{ className: "yte-maximized-video", selector: "video.html5-main-video" },
+		{ className: "yte-maximized-video-container", selector: "div#movie_player" },
+		{ className: "yte-maximized-chrome-bottom", selector: "div.ytp-chrome-bottom" },
+		{ className: "yte-maximized-chapter-hover-container", selector: "div.ytp-chapter-hover-container" },
+		{ className: "yte-maximized-storyboard-framepreview-timestamp", selector: "div.ytp-storyboard-framepreview-timestamp" }
+	];
+	modifyElementsClassList(action, elements);
+}
+function adjustTooltip(action: Action, element: ElementClassPair) {
+	modifyElementClassList(action, element);
+}
+
+export function maximizePlayer() {
+	const videoPlayer = document.querySelector<HTMLVideoElement>("video.html5-main-video");
+	if (!videoPlayer) return;
+	eventManager.addEventListener(videoPlayer, "timeupdate", updateProgressBarPositions, "maximizePlayerButton");
+	const chromeBottom = document.querySelector<HTMLDivElement>("div.ytp-chrome-bottom");
+	if (!chromeBottom) return;
+	const leftControls = chromeBottom.querySelector<HTMLDivElement>("div.ytp-left-controls");
+	if (!leftControls) return;
+	const rightControls = chromeBottom.querySelector<HTMLDivElement>("div.ytp-right-controls");
+	if (!rightControls) return;
+	const pipElement = document.querySelector<HTMLButtonElement>("button.ytp-pip-button");
+	const sizeElement = document.querySelector<HTMLButtonElement>("button.ytp-size-button");
+	const miniPlayerElement = document.querySelector<HTMLButtonElement>("button.ytp-miniplayer-button");
+	adjustPlayer("add");
+	[pipElement, sizeElement, miniPlayerElement].forEach((element) => {
+		if (element)
+			eventManager.addEventListener(
+				element,
+				"click",
+				() => {
+					minimizePlayer();
+					const maximizePlayerMenuItem = getFeatureMenuItem("maximizePlayerButton");
+					if (!maximizePlayerMenuItem) return;
+					maximizePlayerMenuItem.ariaChecked = "false";
+				},
+				"maximizePlayerButton"
+			);
+	});
+	[...leftControls.childNodes, ...rightControls.childNodes].forEach((node) => {
+		eventManager.addEventListener(
+			node as HTMLElement,
+			"mouseover",
+			() => adjustTooltip("add", { className: "yte-maximized-tooltip", selector: "div#movie_player > div.ytp-tooltip" }),
+			"maximizePlayerButton"
+		);
+	});
+}
+
+export function minimizePlayer() {
+	adjustPlayer("remove");
 }

VampireChicken12/youtube-enhancer

(cherry picked from commit f97aa3bdbc9b4755861dbdfcb0e96dba4a473d39)

src/features/maximizePlayerButton/utils.ts
@@ -1,9 +1,7 @@
-import type { YouTubePlayerDiv } from "@/src/types";
-
+import { getFeatureIcon } from "@/src/icons";
 import eventManager from "@/src/utils/EventManager";
 
-let wasInTheatreMode = false;
-let setToTheatreMode = false;
+import { getFeatureButton, updateFeatureButtonIcon, updateFeatureButtonTitle } from "../buttonPlacement/utils";
 
 // TODO: get played progress bar to be accurate when maximized from default view
 // TODO: Add event listener that updates scrubber position when maximize button is clicked
@@ -18,106 +16,80 @@ export function updateProgressBarPositions() {
 	const duration = parseInt(seekBar?.ariaValueMax ?? "0") ?? 0;
 	const seekBarWidth = seekBar?.clientWidth ?? 0;
 
-	const scrubberPosition = parseFloat(Math.min((elapsedTime / duration) * seekBarWidth, seekBarWidth).toFixed(3));
-	// TODO: get this working when video is playing
+	const scrubberPosition = Math.ceil(parseFloat(Math.min((elapsedTime / duration) * seekBarWidth, seekBarWidth).toFixed(1)));
 	scrubber.style.transform = `translateX(${scrubberPosition}px)`;
-	// TODO: get all progress bar lists updated when video is playing
 	hoverProgress.style.left = `${scrubberPosition}px`;
 }
-export function setupVideoPlayerTimeUpdate() {
-	const videoElement = document.querySelector<HTMLVideoElement>("video.video-stream.html5-main-video");
-	if (!videoElement) return;
-	const videoPlayerTimeUpdateListener = () => {
-		updateProgressBarPositions();
-	};
-	eventManager.addEventListener(videoElement, "timeupdate", videoPlayerTimeUpdateListener, "maximizePlayerButton");
-}
-export function maximizePlayer() {
-	// Get the video element
-	const videoElement = document.querySelector<HTMLVideoElement>("video.video-stream.html5-main-video");
-	// If video element is not available, return
-	if (!videoElement) return;
-	const videoContainer = document.querySelector<YouTubePlayerDiv>("#movie_player");
-	if (!videoContainer) return;
-	const controlsElement = document.querySelector<HTMLDivElement>("div.ytp-chrome-bottom");
-	if (!controlsElement) return;
-	const sizeElement = document.querySelector("button.ytp-button.ytp-size-button");
-	if (!sizeElement) return;
-	const featureMenu = document.querySelector<HTMLDivElement>("#yte-feature-menu");
-	if (!featureMenu) return;
-	const {
-		childNodes: [svgElement]
-	} = sizeElement;
-	if (!svgElement || !(svgElement instanceof SVGElement)) return;
-	const theaterModeVariables = {
-		ariaLabel: "Theater mode keyboard shortcut t",
-		dataTitleNoTooltip: "Theater mode",
-		pathD: "m 28,11 0,14 -20,0 0,-14 z m -18,2 16,0 0,10 -16,0 0,-10 z",
-		title: "Theater mode (t)"
-	};
-	const defaultModeVariables = {
-		ariaLabel: "Default view keyboard shortcut t",
-		dataTitleNoTooltip: "Default view",
-		pathD: "m 26,13 0,10 -16,0 0,-10 z m -14,2 12,0 0,6 -12,0 0,-6 z",
-		title: "Default view (t)"
-	};
-	const {
-		childNodes: [, svgPath]
-	} = svgElement;
-	if (!svgPath || !(svgPath instanceof SVGPathElement)) return;
-	if (svgPath.getAttribute("d") === theaterModeVariables.pathD) wasInTheatreMode = true;
-	else wasInTheatreMode = false;
-	if (wasInTheatreMode) setToTheatreMode = false;
-	else setToTheatreMode = true;
-	// TODO: finish this code to make the maximize player button work properly. (implement ytp-scrubber-container adjustment) if all else fails revert to using the theatre mode button to get the tooltips in the correct place
-	console.log(`setToTheatreMode && wasInTheatreMode: ${setToTheatreMode && wasInTheatreMode}`);
-	console.log(`setToTheatreMode && !wasInTheatreMode: ${setToTheatreMode && !wasInTheatreMode}`);
-	console.log(`!setToTheatreMode && wasInTheatreMode: ${!setToTheatreMode && wasInTheatreMode}`);
-	console.log(`!setToTheatreMode && !wasInTheatreMode: ${!setToTheatreMode && !wasInTheatreMode}`);
-	if (
-		videoContainer.classList.contains("maximized_video_container") &&
-		videoElement.classList.contains("maximized_video") &&
-		controlsElement.classList.contains("maximized_controls")
-	) {
-		// sizeElement.click();
-		// if (setToTheatreMode && wasInTheatreMode) {
-		// 	const { dataTitleNoTooltip, title, ariaLabel, pathD } = defaultModeVariables;
-		// 	svgPath.setAttributeNS(null, "d", pathD);
-		// 	sizeElement.setAttributeNS(null, "aria-label", ariaLabel);
-		// 	sizeElement.dataset.titleNoTooltip = dataTitleNoTooltip;
-		// 	sizeElement.title = title;
-		// } else if (setToTheatreMode && !wasInTheatreMode) {
-		// 	const { dataTitleNoTooltip, title, ariaLabel, pathD } = theaterModeVariables;
-		// 	svgPath.setAttributeNS(null, "d", pathD);
-		// 	sizeElement.setAttributeNS(null, "aria-label", ariaLabel);
-		// 	sizeElement.dataset.titleNoTooltip = dataTitleNoTooltip;
-		// 	sizeElement.title = title;
-		// }
-		document.body.style.overflow = "";
-		videoElement.classList.remove("maximized_video");
-		videoContainer.classList.remove("maximized_video_container");
-		controlsElement.classList.remove("maximized_controls");
-		featureMenu.style.bottom = "";
-	} else {
-		// sizeElement.click();
 
-		// if (!setToTheatreMode && wasInTheatreMode) {
-		// 	const { dataTitleNoTooltip, title, ariaLabel, pathD } = defaultModeVariables;
-		// 	svgPath.setAttributeNS(null, "d", pathD);
-		// 	sizeElement.setAttributeNS(null, "aria-label", ariaLabel);
-		// 	sizeElement.dataset.titleNoTooltip = dataTitleNoTooltip;
-		// 	sizeElement.title = title;
-		// } else if (setToTheatreMode && !wasInTheatreMode) {
-		// 	const { dataTitleNoTooltip, title, ariaLabel, pathD } = theaterModeVariables;
-		// 	svgPath.setAttributeNS(null, "d", pathD);
-		// 	sizeElement.setAttributeNS(null, "aria-label", ariaLabel);
-		// 	sizeElement.dataset.titleNoTooltip = dataTitleNoTooltip;
-		// 	sizeElement.title = title;
-		// }
-		document.body.style.overflow = "hidden";
-		videoElement.classList.add("maximized_video");
-		videoContainer.classList.add("maximized_video_container");
-		controlsElement.classList.add("maximized_controls");
-		featureMenu.style.bottom = "-14px";
-	}
+type Action = "add" | "remove";
+type ElementClassPair = { className: string; selector: string };
+function modifyElementClassList(action: Action, elementPair: ElementClassPair) {
+	const { className, selector } = elementPair;
+	const element = document.querySelector<HTMLElement>(selector);
+	if (!element) return;
+	element.classList[action](className);
+}
+function modifyElementsClassList(action: Action, elements: ElementClassPair[]) {
+	elements.forEach((element) => modifyElementClassList(action, element));
+}
+function adjustPlayer(action: Action) {
+	const elements: ElementClassPair[] = [
+		{ className: "yte-maximized-video", selector: "video.html5-main-video" },
+		{ className: "yte-maximized-video-container", selector: "div#movie_player" },
+		{ className: "yte-maximized-chrome-bottom", selector: "div.ytp-chrome-bottom" },
+		{ className: "yte-maximized-chapter-hover-container", selector: "div.ytp-chapter-hover-container" },
+		{ className: "yte-maximized-storyboard-framepreview-timestamp", selector: "div.ytp-storyboard-framepreview-timestamp" }
+	];
+	modifyElementsClassList(action, elements);
+}
+function adjustTooltip(action: Action, element: ElementClassPair) {
+	modifyElementClassList(action, element);
+}
+
+export function maximizePlayer() {
+	const videoPlayer = document.querySelector<HTMLVideoElement>("video.html5-main-video");
+	if (!videoPlayer) return;
+	eventManager.addEventListener(videoPlayer, "timeupdate", updateProgressBarPositions, "maximizePlayerButton");
+	const chromeBottom = document.querySelector<HTMLDivElement>("div.ytp-chrome-bottom");
+	if (!chromeBottom) return;
+	const leftControls = chromeBottom.querySelector<HTMLDivElement>("div.ytp-left-controls");
+	if (!leftControls) return;
+	const rightControls = chromeBottom.querySelector<HTMLDivElement>("div.ytp-right-controls");
+	if (!rightControls) return;
+	const pipElement = document.querySelector<HTMLButtonElement>("button.ytp-pip-button");
+	const sizeElement = document.querySelector<HTMLButtonElement>("button.ytp-size-button");
+	const miniPlayerElement = document.querySelector<HTMLButtonElement>("button.ytp-miniplayer-button");
+	adjustPlayer("add");
+	[pipElement, sizeElement, miniPlayerElement].forEach((element) => {
+		if (element)
+			eventManager.addEventListener(
+				element,
+				"click",
+				() => {
+					minimizePlayer();
+					const maximizePlayerButton = getFeatureButton("maximizePlayerButton");
+					if (!maximizePlayerButton) return;
+					maximizePlayerButton.ariaChecked = "false";
+					const button = getFeatureButton("maximizePlayerButton");
+					const icon = getFeatureIcon("maximizePlayerButton", "shared_icon_position");
+					if (button && button instanceof HTMLButtonElement) {
+						if (typeof icon === "object" && "off" in icon && "on" in icon) updateFeatureButtonIcon(button, icon.off);
+						updateFeatureButtonTitle("maximizePlayerButton", window.i18nextInstance.t("pages.content.features.maximizePlayerButton.toggle.off"));
+					}
+				},
+				"maximizePlayerButton"
+			);
+	});
+	[...leftControls.childNodes, ...rightControls.childNodes].forEach((node) => {
+		eventManager.addEventListener(
+			node as HTMLElement,
+			"mouseover",
+			() => adjustTooltip("add", { className: "yte-maximized-tooltip", selector: "div#movie_player > div.ytp-tooltip" }),
+			"maximizePlayerButton"
+		);
+	});
+}
+
+export function minimizePlayer() {
+	adjustPlayer("remove");
 }